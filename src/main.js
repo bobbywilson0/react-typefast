5164d5b2db45ed27fb8679468bef818d896aa565
Bobby Wilson <bobbywilson0@gmail.com>
2014-09-26 09:37:45 -0600

    change routing for defaulting to lobby

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
 var Routes = Router.Routes;
-var Link = Router.Link;
 var Lobby = require('./lobby');
 var Game = require('./game')
 
 
 var routes = (
   <Routes>
-    <Route name="lobby" path="/lobby" handler={Lobby} />
+    <Route path="/" handler={Lobby} />
     <Route name="game" path="/game/:gameId" handler={Game} />
   </Routes>
 );
